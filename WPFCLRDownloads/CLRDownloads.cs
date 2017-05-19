ee26b8b32c1d3aa4b2532b70b7affbf80235809b
Will McCall <dev@willmccall.com>
2017-05-19 00:00:47 -0400

    Initial Commit

---
+++ WPFCLRDownloads/CLRDownloads.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using Microsoft.VisualBasic;
+
+namespace CLRDownloads
+{
+    class Remover
+    {
+        string location = "";
+        string store = "";
+        string storeLoc = "";
+        bool moved;
+        static int[] dayEnabled = {0,0,0,0,0,0,0};
+
+        public Remover()
+        {
+            location = @"d:\Downloads";
+            store = "$ Removed " + string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}", DateTime.Now);
+            storeLoc = location + "\\" + store;
+            moved = false;
+        }
+
+        public void run()
+        {
+            DateTime now = DateTime.Now;
+            DateTime before = DateTime.Now;
+            //while (true)
+            //{
+            //    now = DateTime.Now;
+            //    if (now.Date > before.Date && now.Hour > 4)
+            //    {
+                    Directory.CreateDirectory(storeLoc);
+                    remove(location, now);
+                    before = now;
+                    setStore();
+                    setLoc();
+                //}
+                //Thread.Sleep(300000);
+            //}
+        }
+
+        public void remove(string path, DateTime now)
+        {
+            string[] folders = Directory.GetDirectories(path);
+            string filename;
+            string fileLoc;
+            foreach (string file in Directory.GetFiles(path))
+            {
+                filename = file.Remove(0, path.Length);
+                fileLoc = storeLoc + "\\" + filename;
+                Directory.Move(file, fileLoc);
+                moved = true;
+            }
+            foreach (string folder in folders)
+            {
+                if (!folder.Contains(location + "\\$ Removed "))
+                {
+                    filename = folder.Remove(0, location.Length);
+                    fileLoc = storeLoc + filename;
+                    Directory.Move(folder, fileLoc);
+                    moved = true;
+                }
+            }
+            if (!moved)
+            {
+                Directory.Delete(storeLoc);
+            }
+            else
+            {
+                Console.WriteLine("Removed @: " + now);
+            }
+        }
+
+        private void setStore()
+        {
+            store = "$ Removed " + string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}", DateTime.Now);
+        }
+
+        private void setLoc()
+        {
+            storeLoc = location + "\\" + store;
+        }
+
+        public static void setDayEnabled(int day, int val)
+        {
+            dayEnabled[day] = val;
+        }
+    }
+}
---
+++ WPFCLRDownloads/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CLRDownloads;
+
+namespace WPFCLRDownloads
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, int> days = new Dictionary<string, int>
+        {
+            {"Monday"   , 0 },
+            {"Tuesday"  , 1 },
+            {"Wednesday", 2 },
+            {"Thursday" , 3 },
+            {"Friday"   , 4 },
+            {"Saturday" , 5 },
+            {"Sunday"   , 6 }
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            Remover r = new Remover();
+            r.run();
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Handle(sender as CheckBox);
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Handle(sender as CheckBox);
+        }
+
+        private void Handle(CheckBox checkbox)
+        {
+            int day = days[checkbox.Name];
+            if ((bool)checkbox.IsChecked)
+            {
+                CLRDownloads.Remover.setDayEnabled(day, 1);
+            }
+            else
+            {
+                CLRDownloads.Remover.setDayEnabled(day, 0);
+            }
+            
+        }
+    }
+}
